e2d9dbfedb3b94ae63bf40a04a79df07117ae927
Hyrum-Jones <jon23079@byui.edu>
2026-01-22 15:21:07 -0700

    finished Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,51 @@
 using System;
 
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+            userNumber = int.Parse(input);
+
+            if (userNumber != 0)
+            {
+               numbers.Add(userNumber); 
+            }
+            
+        }
+
+        float sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        float average = sum / numbers.Count;
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+
     }
 }
